6a6ed7d481338986da48762fc5b9d82bb0330350
xinwei47 <xinwei47@gmail.com>
2021-09-21 22:44:11 -0500

    add: label annotation toggle

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -11,6 +11,22 @@ const flashBtnClose = document.querySelector('.flash__btn');
 // advanced search button to toggle the additional criteria
 advancedToggle.addEventListener('click', () => toggleClass(advancedSearchBlock, 'hidden'));
 
+// show "min" and "max" annotation
+const labelMin = document.querySelector('.search__label-icon--min');
+const labelMax = document.querySelector('.search__label-icon--max');
+const annotationMin = document.querySelector('.search__annotation-box--min');
+const annotationMax = document.querySelector('.search__annotation-box--max');
+
+labelMin.addEventListener('click', () => {
+    toggleClass(annotationMin, 'hidden');
+    addClass(annotationMax, 'hidden');
+});
+
+labelMax.addEventListener('click', () => {
+    toggleClass(annotationMax, 'hidden');
+    addClass(annotationMin, 'hidden');
+});
+
 // flash btn to close the flash block
 if (flashBtnClose) {
     flashBtnClose.addEventListener('click', () => addClass(flash, 'hidden'));
